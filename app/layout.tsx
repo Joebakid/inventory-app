bd2273e98d3d796d2c6939b24cf4847460c8b268
joebakid <josephbawo@gmail.com>
2025-08-17 19:54:03 +0100

    env update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'success frozen food',
+  title: 'success frozen foods',
   description: 'an inventory app',
   
 }
