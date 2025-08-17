c9f5f08e22fbb1bda91ee70879934e0a971f60ba
joebakid <josephbawo@gmail.com>
2025-08-17 19:50:21 +0100

    url

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  description: 'an inventory app',
+  
 }
 
 export default function RootLayout({
